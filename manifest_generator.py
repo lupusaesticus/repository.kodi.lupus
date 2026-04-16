3fc6d930623631afa7ad43c69fc7410b6a357fb8
lupus <lupusaesticus@gmail.com>
2026-04-16 10:19:08 +0100

    fixed folder structure

--- manifest_generator.py
+++ manifest_generator.py
@@ -1,7 +1,9 @@
 import os
 import hashlib
+import shutil
 
 def generate_manifest():
+    DEST_FOLDER = "repository.kodi.lupus"
     xml_content = u'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<addons>\n'
     for root, dirs, files in os.walk("."):
         for file in files:
@@ -18,6 +20,15 @@ def generate_manifest():
     with open("addons.xml.md5", "w", encoding="utf-8") as f:
         f.write(md5_hash)
     print("Manifest generated successfully.")
+
+    # 3. Move them to the repo subfolder
+    if not os.path.exists(DEST_FOLDER):
+        os.makedirs(DEST_FOLDER)
+        
+    shutil.move("addons.xml", os.path.join(DEST_FOLDER, "addons.xml"))
+    shutil.move("addons.xml.md5", os.path.join(DEST_FOLDER, "addons.xml.md5"))
+    
+    print(f"Manifest and MD5 successfully moved to {DEST_FOLDER}/")
 
 if __name__ == "__main__":
     generate_manifest()
